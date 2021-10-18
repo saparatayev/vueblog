4e713ba87532c4690e1be4f037c7a1260daf1308
saparatayev <sopa.97@mail.ru>
2021-10-18 11:31:55 +0500

    set up a hero section

---
+++ src/views/Home/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-full">
+        <img src="/images/hero.jpg" alt="">
+        <div>
+            <div>welcome to laratips</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non aliquid optio sequi at voluptate nam temporibus placeat recusandae dolore! A sint dignissimos error cum itaque illum nulla optio necessitatibus!</div>
+            <div>
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 17l-4 4m0 0l-4-4m4 4V3"></path></svg>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Hero />
+    <div>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quos numquam suscipit autem quidem. Veniam harum repudiandae atque. Labore explicabo earum ullam iste ex ab assumenda vitae temporibus magnam consequuntur!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quos numquam suscipit autem quidem. Veniam harum repudiandae atque. Labore explicabo earum ullam iste ex ab assumenda vitae temporibus magnam consequuntur!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quos numquam suscipit autem quidem. Veniam harum repudiandae atque. Labore explicabo earum ullam iste ex ab assumenda vitae temporibus magnam consequuntur!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Hero from './Hero.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Hero,
+  }
+}
+</script>
