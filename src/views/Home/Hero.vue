db6ba1bf20b6a5e1ef5137f08fd6af48ab9b3a7b
saparatayev <sopa.97@mail.ru>
2022-03-01 09:32:10 +0500

    fix: styles for hero section

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="h-full">
-        <img src="/images/hero.jpg" alt="">
-        <div>
-            <div>welcome to laratips</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non aliquid optio sequi at voluptate nam temporibus placeat recusandae dolore! A sint dignissimos error cum itaque illum nulla optio necessitatibus!</div>
-            <div>
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 17l-4 4m0 0l-4-4m4 4V3"></path></svg>
+    <div class="relative z-0 hero-height">
+        <img src="/images/hero.jpg" alt="hero"
+        class="block w-full h-full object-cover">
+        
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+
+        <div class="px-4 sm:px-0 absolute inset-0 text-white">
+            <div class="container mx-auto h-full flex flex-col justify-center">
+                <div>Welcome to</div>
+                <div class="mt-1 text-6xl font-bold">Laratips</div>
+                <div class="mt-2 sm:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non aliquid optio sequi at voluptate nam temporibus placeat recusandae dolore! A sint dignissimos error cum itaque illum nulla optio necessitatibus!</div>
+                <div class="mt-8 text-primary">
+                    <svg class="h-10 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 17l-4 4m0 0l-4-4m4 4V3"></path></svg>
+                </div>
             </div>
         </div>
     </div>
