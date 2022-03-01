bcda343ef3b20a8696dc7f5f71ca8838361a2214
saparatayev <sopa.97@mail.ru>
2022-03-01 11:37:20 +0500

    frontend footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div>
     <Navbar />
     <router-view/>
-    <footer>
-      Footer
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/layouts/Navbar.vue'
+import Footer from '@/components/layouts/Footer.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }  
 }
 </script>
